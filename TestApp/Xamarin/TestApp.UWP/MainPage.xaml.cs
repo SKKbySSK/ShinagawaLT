a1e6c9bcd57f39b9d62e303ee89f044534119191
Kaisei Sunaga from mac <jkdwhssk@yahoo.co.jp>
2018-08-05 15:16:42 +0900

    Add WPF and UWP projects

---
+++ TestApp/Xamarin/TestApp.UWP/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Xamarin.Forms.Platform.UWP;
+
+// 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
+
+namespace TestApp.UWP
+{
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class MainPage : WindowsPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            LoadApplication(new TestApp.App());
+        }
+    }
+}
---
+++ TestApp/Xamarin/TestApp.macOS/AppDelegate.cs
@@ -0,0 +1,34 @@
+﻿using AppKit;
+using Foundation;
+
+namespace TestApp.macOS
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : Xamarin.Forms.Platform.MacOS.FormsApplicationDelegate
+    {
+        public AppDelegate()
+        {
+            var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+
+            var rect = new CoreGraphics.CGRect(200, 200, 800, 800);
+            MainWindow = new NSWindow(rect, style, NSBackingStore.Buffered, false)
+            {
+                Title = "TestApp"
+            };
+        }
+
+        public override NSWindow MainWindow { get; }
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            global::Xamarin.Forms.Forms.Init();
+            LoadApplication(new App());
+            base.DidFinishLaunching(notification);
+        }
+
+        public override void WillTerminate(NSNotification notification)
+        {
+            // Insert code here to tear down your application
+        }
+    }
+}
